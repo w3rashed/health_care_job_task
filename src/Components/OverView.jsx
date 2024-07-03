ab3c00bbbb710eeeedef89aa161b031532a340c8
Rashed <w3rashed@gmail.com>
2024-07-03 18:00:19 +0600

    overView section right side done

--- src/Components/OverView.jsx
+++ src/Components/OverView.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import pieChart from "../../public/image/pieChart.png";
+import videoIcon from "../../public/image/video icon.png";
 import Image from "next/image";
 
 const OverView = () => {
   return (
-    <div className="w-full">
-        {/*  left */}
+    <div className="flex">
+      {/*  left */}
       <div className="w-[216px] h-[312px] border p-5 rounded-3xl flex-1">
         <div>
-          <h2 className="font-semibold text-4xl">90%</h2>
+          <h2 className="font-semibold text-[40px]">90%</h2>
           <p className="text-sm mt-2 mb-6">
             Patient satisfaction rate, reflecting our commitment.
           </p>
@@ -16,14 +17,13 @@ const OverView = () => {
         <Image width={130} height={130} src={pieChart} alt="" />
       </div>
       <div></div>
+      {/* right */}
       <div className="w-[216px] h-[312px] border p-5 rounded-3xl flex-1">
         <div>
-          <h2 className="font-semibold text-4xl">90%</h2>
-          <p className="text-sm mt-2 mb-6">
-            Patient satisfaction rate, reflecting our commitment.
-          </p>
+          <h2 className="font-semibold text-[40px]">50+</h2>
+          <p className="text-sm mt-2 mb-6">Free lession video for patient</p>
         </div>
-        <Image width={130} height={130} src={pieChart} alt="" />
+        <Image width={130} height={130} src={videoIcon} alt="" />
       </div>
     </div>
   );
