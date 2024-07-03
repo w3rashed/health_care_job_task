78608b02be0c312dabbc0f49535e9816d171a749
Rashed <w3rashed@gmail.com>
2024-07-03 17:46:26 +0600

    overView section left side done

--- src/Components/OverView.jsx
+++ src/Components/OverView.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import pieChart from "../../public/image/pieChart.png";
+import Image from "next/image";
 
 const OverView = () => {
-    return (
+  return (
+    <div className="w-full">
+        {/*  left */}
+      <div className="w-[216px] h-[312px] border p-5 rounded-3xl flex-1">
         <div>
-            
+          <h2 className="font-semibold text-4xl">90%</h2>
+          <p className="text-sm mt-2 mb-6">
+            Patient satisfaction rate, reflecting our commitment.
+          </p>
         </div>
-    );
+        <Image width={130} height={130} src={pieChart} alt="" />
+      </div>
+      <div></div>
+      <div className="w-[216px] h-[312px] border p-5 rounded-3xl flex-1">
+        <div>
+          <h2 className="font-semibold text-4xl">90%</h2>
+          <p className="text-sm mt-2 mb-6">
+            Patient satisfaction rate, reflecting our commitment.
+          </p>
+        </div>
+        <Image width={130} height={130} src={pieChart} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default OverView;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import NavMenu from "@/Components/NavMenu";
-import Image from "next/image";
+"use client";
+import OverView from "@/Components/OverView";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="">
+      <OverView></OverView>
     </main>
   );
 }
